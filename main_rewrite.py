0d017a21db2301860e10fd0faa1c26f2162f2cd8
usun3N <unconfigured@null.spigotmc.org>
2025-01-07 19:56:53 +0900

    add join_request

--- main_rewrite.py
+++ main_rewrite.py
@@ -31,6 +31,26 @@ class Embed_Tool:
             embed.color = discord.Color.red()
         return embed
 
+    def join_request_embed(request_id: str):
+        join_request = database.get_join_request(request_id)
+        request_id, build_id, category_name, sender_id, sender_name, status, processor_user_id, processor_user_name = join_request
+        embed = discord.Embed(title="Join Request")
+        embed.add_field(name="リクエストID", value=request_id, inline=False)
+        embed.add_field(name="ビルドID", value=build_id, inline=False)
+        embed.add_field(name="カテゴリー名", value=category_name, inline=False)
+        embed.add_field(name="送信者", value=sender_name, inline=False)
+        embed.add_field(name="送信者ID", value=sender_id, inline=False)
+        embed.add_field(name="状態", value=status, inline=False)
+        embed.add_field(name="担当者", value=processor_user_name, inline=False)
+        embed.add_field(name="担当者ID", value=processor_user_id, inline=False)
+        if status == "待機中":
+            embed.color = discord.Color.blurple()
+        elif status == "承認":
+            embed.color = discord.Color.green()
+        elif status == "拒否":
+            embed.color = discord.Color.red()
+        return embed
+    
     def embed_to_dict(embed: discord.Embed):
         embed_dict = embed.to_dict()
         result = {}
@@ -51,7 +71,7 @@ class DataBase:
         cur.execute("CREATE TABLE IF NOT EXISTS build_request(request_id TEXT PRIMARY KEY, category_name TEXT, sender_id INTEGER, sender_name TEXT, status TEXT, processor_user_id INTEGER, processor_name TEXT)")
         cur.execute("CREATE TABLE IF NOT EXISTS category(build_id TEXT PRIMARY KEY, category_id INTEGER, guild_id INTEGER, name TEXT, moderator_channel_id INTEGER, owner_id INTEGER, role_id INTEGER)")
         cur.execute("CREATE TABLE IF NOT EXISTS guild_settings(guild_id INTEGER PRIMARY KEY, moderator_channel_id INTEGER, notification_channel_id INTEGER, moderator_role_id INTEGER)")
-        cur.execute("CREATE TABLE IF NOT EXISTS join_request(request_id INTEGER PRIMARY KEY, build_id TEXT, sender_id INTEGER, status TEXT, processor_user_id INTEGER)")
+        cur.execute("CREATE TABLE IF NOT EXISTS join_request(request_id TEXT PRIMARY KEY, build_id TEXT, category_name TEXT, sender_id INTEGER, sender_name TEXT, status TEXT, processor_user_id INTEGER, processor_name TEXT)")
     
     def add_build_request(self, request_id: str, category_name: str, sender_id: int, sender_name: str, status: str, processor_user_id: int, processor_name: str):
         cur = self.db.cursor()
@@ -71,15 +91,15 @@ class DataBase:
         cur.execute(sql)
         self.db.commit()
     
-    def add_join_request(self, request_id: str, build_id: str, sender_id: int, status: str, processor_user_id: int):
+    def add_join_request(self, request_id: str, build_id: str, category_name: str, sender_id: int, sender_name: str, status: str, processor_user_id: int, processor_user_name: str):
         cur = self.db.cursor()
-        sql = f"INSERT INTO join_request VALUES ('{request_id}', '{build_id}', {sender_id}, '{status}', {processor_user_id})"
+        sql = f"INSERT INTO join_request VALUES ('{request_id}', '{build_id}', '{category_name}',{sender_id}, '{sender_name}', '{status}', {processor_user_id}, '{processor_user_name}')"
         cur.execute(sql)
         self.db.commit()
 
-    def update_status_join_request(self, request_id: str, processor_user_id: int, status: str):
+    def update_status_join_request(self, request_id: str, processor_user_id: int, processor_user_name: str, status: str):
         cur = self.db.cursor()
-        sql = f"UPDATE join_request SET status = '{status}', processor_user_id = {processor_user_id} WHERE request_id = '{request_id}'"
+        sql = f"UPDATE join_request SET status = '{status}', processor_user_id = {processor_user_id}, processor_name = '{processor_user_name}' WHERE request_id = '{request_id}'"
         cur.execute(sql)
         self.db.commit()
     
@@ -112,6 +132,12 @@ class DataBase:
         sql = f"SELECT * FROM category WHERE build_id = '{build_id}'"
         cur.execute(sql)
         return cur.fetchone()
+    
+    def get_category_with_role_id(self, guild_id: int, role_id: int):
+        cur = self.db.cursor()
+        sql = f"SELECT * FROM category WHERE guild_id = {guild_id} AND role_id = {role_id}"
+        cur.execute(sql)
+        return cur.fetchone()
 
     def get_guild_settings(self, guild_id: int):
         cur = self.db.cursor()
@@ -119,11 +145,18 @@ class DataBase:
         cur.execute(sql)
         return cur.fetchone()
     
-    def get_request_list(self):
+    def get_build_request_list(self):
         cur = self.db.cursor()
         sql = f"SELECT request_id, category_name, sender_name FROM build_request WHERE status = '待機中'"
         cur.execute(sql)
         return cur.fetchall()
+
+    def get_join_request_list(self):
+        cur = self.db.cursor()
+        sql = f"SELECT request_id, category_name, sender_name FROM join_request WHERE status = '待機中'"
+        cur.execute(sql)
+        return cur.fetchall()
+    
     def close(self):
         self.db.close()
     
@@ -159,7 +192,7 @@ class BuildRequestView(discord.ui.View):
         moderator_channel = await guild.create_text_channel(name="管理者用", category=category, overwrites=moderator_overwrites)
         await general_channel.send(f"作成が完了しました")
         await moderator_channel.send(f"{sender.mention} 参加申請があった場合はこのチャンネルに通知されます。このチャンネルはこのカテゴリーの管理者にのみ表示されます。")
-        sender.add_roles(new_role)
+        await sender.add_roles(new_role)
 
         guild_data = interaction.guild
         database.add_category(request_id, category.id, guild_data.id, category_name, moderator_channel.id, sender_id, new_role.id)
@@ -180,11 +213,31 @@ class BuildRequestView(discord.ui.View):
 class JoinRequestView(discord.ui.View):
     @discord.ui.button(label="Accept",row=0, style=discord.ButtonStyle.green)
     async def accept_button_callback(self, button: discord.ui.Button ,interaction: discord.Interaction):
-        pass
+        message = interaction.message
+        embed_data = Embed_Tool.embed_to_dict(message.embeds[0])
+        request_id = embed_data["リクエストID"]
+        database.update_status_join_request(request_id, interaction.user.id, interaction.user.display_name, "承認")
+        request_id, build_id, category_name, sender_id, sernder_name, status, processor_user_id, processor_user_name = database.get_join_request(request_id)
+        category = database.get_category(build_id)
+        build_id, category_id, guild_id, name, moderator_channel_id, owner_id, role_id = category
+        role = interaction.guild.get_role(role_id)
+        sender = interaction.guild.get_member(sender_id)
+        embed = Embed_Tool.join_request_embed(request_id)
+        await sender.add_roles(role)
+        await sender.send("Join Request Accepted", embed=embed)
+        await interaction.message.edit("", embed=embed, view=None)
 
     @discord.ui.button(label="Deny", row=1, style=discord.ButtonStyle.red)
     async def deny_button_callback(self, button: discord.ui.Button ,interaction: discord.Interaction):
-        pass
+        message = interaction.message
+        embed_data = Embed_Tool.embed_to_dict(message.embeds[0])
+        request_id = embed_data["リクエストID"]
+        database.update_status_join_request(request_id, interaction.user.id, interaction.user.display_name, "拒否")
+        request_id, category_name, sender_id, sernder_name, status, processor_user_id, processor_user_name = database.get_join_request(request_id)
+        embed = Embed_Tool.join_request_embed(request_id)
+        sender = interaction.guild.get_member(sender_id)
+        await sender.send("Join Request Denied", embed=embed)
+        await interaction.message.edit("", embed=embed, view=None)
     
     
 @bot.event
@@ -194,60 +247,86 @@ async def on_ready():
 @bot.slash_command(guild_ids=guild_ids)
 async def hello(ctx: discord.ApplicationContext):
     await ctx.respond("Hello!")
-
-# @bot.slash_command(guild_ids=guild_ids)
-# async def join_request(ctx: discord.ApplicationContext, role: discord.Role):
-#     await ctx.respond(f"Join Request for {role.name} sent", ephemeral=True)
-#     category_data = data.get_category_with_role_id(ctx.guild_id, role.id)
-#     if not category_data:
-#         await ctx.respond("Category not found", ephemeral=True)
-#         return
-#     category_id = category_data["category"]
-#     owner_id = category_data["owner"]
-#     moderator_channel_id = category_data.get("moderator_channel", None)
-#     if not category_id or not moderator_channel_id or not owner_id:
-#         await ctx.respond("エラーが発生しました", ephemeral=True)
-#         return
-#     owner = bot.get_user(owner_id)
-#     moderator_channel = bot.get_channel(moderator_channel_id)
-#     await moderator_channel.send(f"{owner.mention}{ctx.author.display_name} さんが参加申請を送りました。", view=JoinRequestView())
+    
+@bot.slash_command(guild_ids=guild_ids)
+async def join_request(ctx: discord.ApplicationContext, role: discord.Role):
+    guild_settings = database.get_guild_settings(ctx.guild_id)
+    if guild_settings is None:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
+    category = database.get_category_with_role_id(ctx.guild_id, role.id)
+    if category is None:
+        await ctx.respond("Category not found", ephemeral=True)
+        return
+    if ctx.author.get_role(role.id):
+        await ctx.respond("You already have this role", ephemeral=True)
+        return
+    #build_id TEXT PRIMARY KEY, category_id INTEGER, guild_id INTEGER, name TEXT, moderator_channel_id INTEGER, owner_id INTEGER, role_id INTEGE
+    build_id, category_id, guild_id, name, moderator_channel_id, owner_id, role_id = category
+    request_id = str(uuid4())
+    database.add_join_request(request_id, build_id, name, ctx.author.id, ctx.author.display_name, "待機中", 0, "---")
+    guild = ctx.guild
+    embed = Embed_Tool.join_request_embed(request_id)
+    await guild.get_channel(moderator_channel_id).send(f"{ctx.guild.get_member(owner_id).mention}", embed=embed, view=JoinRequestView())
+    await ctx.respond(f"Join request sent to {role.name}", ephemeral=True)
+    
+    
 
 @bot.slash_command(guild_ids=guild_ids)
 async def set_moderator_channel(ctx: discord.ApplicationContext, channel: discord.TextChannel):
+    guild_settings = database.get_guild_settings(ctx.guild_id)
+    if guild_settings is None:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
+    _, _, _, moderator_role = guild_settings
+    if not ctx.author.get_role(moderator_role):
+        await ctx.respond("You are not authorized to use this command", ephemeral=True)
+        return
     database.update_guild_settings("moderator_channel", ctx.guild_id, channel.id)
     await ctx.respond(f"Channel set to {channel.id}", ephemeral=True)
 
 
 @bot.slash_command(guild_ids=guild_ids)
 async def set_notification_channel(ctx: discord.ApplicationContext, channel: discord.TextChannel):
+    guild_settings = database.get_guild_settings(ctx.guild_id)
+    if guild_settings is None:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
+    _, _, _, moderator_role = guild_settings
+    if not ctx.author.get_role(moderator_role):
+        await ctx.respond("You are not authorized to use this command", ephemeral=True)
+        return
     database.update_guild_settings("notification_channel", ctx.guild_id, channel.id)
     await ctx.respond(f"Channel set to {channel.id}", ephemeral=True)
 
 @bot.slash_command(guild_ids=guild_ids)
-async def test_send(ctx: discord.ApplicationContext, message: str):
-    guild_settings = database.get_guild_settings(ctx.guild_id)
-    _, moderation_channel_id, _, _= guild_settings
-    if not moderation_channel_id:
-        await ctx.respond("No channel set")
+async def set_moderator_role(ctx: discord.ApplicationContext, role: discord.Role):
+    if not ctx.author.guild_permissions.manage_roles:
+        await ctx.respond("You are not authorized to use this command", ephemeral=True)
         return
-    channel = bot.get_channel(moderation_channel_id)
-    await channel.send(message)
-    await ctx.respond("Message sent", ephemeral=True)
+    database.update_guild_settings("moderator_role", ctx.guild_id, role.id)
+    await ctx.respond(f"Role set to {role.id}", ephemeral=True)
 
 @bot.slash_command(guild_ids=guild_ids)
 async def set_guild_settings(ctx: discord.ApplicationContext, moderation_channel: discord.TextChannel, notification_channel: discord.TextChannel, moderation_role: discord.Role):
+    guild_setting = database.get_guild_settings(ctx.guild_id)
+    if guild_setting is not None:
+        await ctx.respond("Guild settings already set", ephemeral=True)
+        return
     database.add_guild_settings(ctx.guild_id, moderation_channel.id, notification_channel.id, moderation_role.id)
     await ctx.respond("Guild settings set", ephemeral=True)
 
 @bot.slash_command(guild_ids=guild_ids)
 async def build_request(ctx: discord.ApplicationContext, name: str):
     guild_settings = database.get_guild_settings(ctx.guild_id)
-    print(guild_settings)
+    if not guild_settings:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
     _, moderation_channel_id, _, _ = guild_settings
     if not moderation_channel_id:
-        await ctx.respond("No channel set")
+        await ctx.respond("No channel set", ephemeral=True)
         return
-    moderation_channel = bot.get_channel(moderation_channel_id)
+    moderation_channel = ctx.guild.get_channel(moderation_channel_id)
     view = BuildRequestView()
     message = await moderation_channel.send("loading")
     request_id = str(uuid4())
@@ -267,50 +346,60 @@ async def stop(ctx: discord.ApplicationContext):
 
 @bot.slash_command(guild_ids=guild_ids)
 async def get_build_request_list(ctx: discord.ApplicationContext):
+    guild_settings = database.get_guild_settings(ctx.guild_id)
+    if not guild_settings:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
+    _, _, _, moderation_role_id = guild_settings
+    if not moderation_role_id:
+        await ctx.respond("No role set")
+        return
+    if not ctx.author.get_role(moderation_role_id):
+        await ctx.respond("You are not authorized to use this command", ephemeral=True)
+        return
     result = ""
-    for request_id, category_name, sender_name in database.get_request_list():
+    for request_id, category_name, sender_name in database.get_build_request_list():
         result += f"ID: {request_id} \nカテゴリー名: {category_name} \n送信者: {sender_name}\n\n"
     await ctx.respond(result, ephemeral=True)
 
 @bot.slash_command(guild_ids=guild_ids)
-async def get_build_request(ctx: discord.ApplicationContext, request_id: str):
-    request_id, category_name, sender_id, sernder_name, status, processor_user_id, processor_user_name = database.get_build_request(request_id)
-    embed = Embed_Tool.build_request_embed(request_id)
-    await ctx.respond("", embed=embed, ephemeral=True)
-
-@bot.slash_command(guild_ids=guild_ids)
-async def accept(ctx: discord.ApplicationContext, request_id: str):
-    request_id, category_name, sender_id, sernder_name, status, processor_user_id, processor_user_name = database.get_build_request(request_id)
-    _, _, _, moderator_role_id = database.get_guild_settings(ctx.guild_id)
-    if ctx.author.get_role(moderator_role_id) is None:
+async def get_join_request_list(ctx: discord.ApplicationContext):
+    guild_settings = database.get_guild_settings(ctx.guild_id)
+    if not guild_settings:
+        await ctx.respond("Guild settings not found", ephemeral=True)
+        return
+    _, _, _, moderation_role_id = guild_settings
+    if not moderation_role_id:
+        await ctx.respond("No role set")
+        return
+    if not ctx.author.get_role(moderation_role_id):
         await ctx.respond("You are not authorized to use this command", ephemeral=True)
         return
-    guild = ctx.guild
-    sender = guild.get_member(sender_id)
-    database.update_status_build_request(request_id, ctx.author.id, ctx.author.display_name, "承認")
-    embed = Embed_Tool.build_request_embed(request_id)
-    await ctx.respond("", embed=embed, ephemeral=True)
-    new_role = await guild.create_role(name=category_name)
+    result = ""
+    for request_id, build_id, category_name, sender_name in database.get_join_request_list():
+        result += f"ID: {request_id} \nBuild ID: {build_id} \nカテゴリー名: {category_name} \n送信者: {sender_name}\n\n"
+    await ctx.respond(result, ephemeral=True)
 
-    general_overwrites = {
-        guild.default_role: discord.PermissionOverwrite(view_channel=False),
-        new_role: discord.PermissionOverwrite(view_channel=True),
-        sender: discord.PermissionOverwrite(manage_channels=True)
-    }
-    moderator_overwrites = {
-        guild.default_role: discord.PermissionOverwrite(view_channel=False),
-        new_role: discord.PermissionOverwrite(view_channel=False),
-        sender: discord.PermissionOverwrite(view_channel=True, manage_channels=True)
-    }
-
-    category = await guild.create_category(name=category_name, overwrites=general_overwrites)
-    general_channel = await guild.create_text_channel(name="一般", category=category)
-    moderator_channel = await guild.create_text_channel(name="管理者用", category=category, overwrites=moderator_overwrites)
-    await general_channel.send(f"作成が完了しました")
-    await moderator_channel.send(f"{sender.mention} 参加申請があった場合はこのチャンネルに通知されます。このチャンネルはこのカテゴリーの管理者にのみ表示されます。")
-    await sender.add_roles(new_role)
-
-    database.add_category(request_id, category.id, guild.id, category_name, moderator_channel.id, sender_id, new_role.id)
+@bot.slash_command(guild_ids=guild_ids)
+async def help(ctx: discord.ApplicationContext):
+    result = """```
+    /build_request [任意の名前]- Build requestを作成するときに使う
+    -承認されると指定した名前で新しくカテゴリーが作成され、そのカテゴリーの管理権限が渡されます。
+    -同じ名前でロールが作成され、そのロールがついているメンバーにしかそのカテゴリーは表示されません
+    
+    /join_request [任意のロール]- 入りたいところのロールを選んでJoin requestを送るときに使う
+    -入りたいカテゴリーと同じ名前のロールを選択してください。
+    -承認されるとそのロールが付与されます。
+    
+    /set_guild_settings [管理用チャンネル] [通知用チャンネル] [管理者ロール]- サーバーでのbotの初期設定用のコマンド(管理者のみ)
+    /set_moderation_channel [管理用チャンネル]- Build requestが届くチャンネルを設定(管理者のみ)
+    /set_notification_channel [通知用チャンネル]- なにかしらに使う予定のチャンネルを設定(管理者のみ)
+    /set_moderator_role [管理者ロール]- このbotの管理者として扱うロールを設定する(サーバーでのロール管理の権限が必要)
+    /get_build_request_list - 待機中のBuild request一覧(管理者のみ)
+    /get_join_request_list - 待機中のJoin request一覧(管理者のみ)
+    /stop - Botを止める(ユーのみ)```
+    """
+    await ctx.respond(result, ephemeral=True)
 
 database = DataBase()
 bot.run(token)
